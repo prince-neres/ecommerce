0a89993196e6b3a2a052656f25aa4ce2d88f4c0c
Prince Neres <prince84224960@gmail.com>
2023-03-25 21:54:02 -0300

    Create API

---
+++ backend/app/products.py
@@ -0,0 +1,67 @@
+products = [
+    {
+      '_id': '1',
+      'name': 'Monitor de Áudio, Edifier, R1000T4 preta',
+      'image': '/images/R1000T4.jpg',
+      'description':
+        'Monitor de Áudio 24W RMS R1000T4 EDIFIER 2 Bivolt - madeira A EDIFIER R1000T4 possui atualização tecnológica com amplificador melhorado de classe-D, sua potência de 24W RMS fara que aproveite ao máximo o seu som. Acústica Surpreendente Material com fibra de Madeira 100% (MDF), este monitor de áudio é construído para resistir à ressonância. Para ser usado em home studio, em casa ou no escritório, esta é a opção perfeita com o melhor custo benefício do mercado nacional. Uso Semiprofissional Apesar de ser um produto dedicado aqueles que estão a procura dos tons mais limpos e nítidos, a R1000T4 não se direciona apenas a esse grupo. Seja você um entusiasta de jogos ou simplesmente quer dar ainda mais qualidade para os conteúdos multimídia que você reproduz no seu computador, celular ou tablet, esse monitor irá cair como uma luva. Surpreenda-se com os resultados e viva experiências com ainda mais emoção. Som Genuíno e Equilibrado Se você está iniciando seus primeiros projetos com edição de áudio, essa é uma ótima e barata opção de dar os próximos passos. A diferença da Edifier R1000T4 das demais caixas presentes no mercado é que não atua modificando as frequências do áudio, ou seja, é flat o que significa que ele vem com todas as frequências equilibradas, não tem equalização. O que ele reproduzir vai estar o mais próximo possível do gravado, assim você vai ter uma referência melhor para se basear ao equalizar os instrumentos. Um toque de estilo Apresentando drivers de graves de 4″ e drivers de tweeter de diafragma de seda de ¾”, o monitor de áudio EDIFIER R1000T4 é projetado para oferecer som com emoções e detalhes. E o par de drivers de graves dourados adicionam um toque de estilo ao seu ambiente. Dupla Entrada RCA Para uma experiência de áudio personalizada, você encontrará os controles de ajuste de graves e volume na parte de trás deste monitor de áudio. A EDIFIER R1000T4 também possui duas portas de entrada de linha estéreo, com aprimoramento dinâmicos de agudos na porta "A". DADOS TÉCNICOS - Potencia: 24Watts RMS (12Wx2) - Agudos: 1, 2”. - Graves: 4”. - Proteção Magnética: Sim - S/ N Ratio: > 85 dB - Distorção: 0.5% - Frequência response: 75Hz 18KHZ (+/-9dB) - Nível de Entrada: PC: 700mV 50mV AUX: 550mV 50mV - Bivolt - impedância: 4ohm ITENS INCLUSOS 1-Coluna Ativa 1-Coluna Passiva 1-Cabo de ligação das colunas 1-Cabo de ligação de áudio de p2(3.5 mm) para RCA duplo 1-Cabo de áudio (RCA – RCA estéreo) 1-Manual.',
+      'brand': 'Edifier',
+      'category': 'Electronics',
+      'price': 779.99,
+      'countInStock': 5,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Smart TV LED 50" 4K UHD Samsung 50BU8000 - Wifi, USB',
+      'image': '/images/50BU8000.jpg',
+      'description':
+        'Samsung Smart TV 50 polegadas Crystal UHD 4K BU8000 2022, Painel Dynamic Crystal Color, Design slim, Tela sem limites, Visual Livre de Cabos, Alexa built in, Controle Remoto Único.',
+      'brand': 'Samsung',
+      'category': 'Electronics',
+      'price': 2510.99,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Keychron K4',
+      'image': '/images/keychronk4.jpg',
+      'description':
+        'Keychron K4 Teclado mecânico para jogos sem fio Bluetooth/USB com fio, compacto, 100 teclas, RGB LED, retroiluminado, Gateron G Pro Brown Switch N-Key, moldura de alumínio para Mac Windows, versão 2',
+      'brand': 'Keychrons',
+      'category': 'Electronics',
+      'price': 849.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'Sony Playstation 5',
+      'image': '/images/ps5.jpg',
+      'description':
+        'Experimente o carregamento extremamente rápido do SSD de ultra-velocidade, uma imersão mais profunda com suporte à resposta tátil, gatilhos adaptáveis e áudio 3D. Além de uma geração totalmente nova de jogos incríveis. O console PS5 oferece novas possibilidades de jogabilidade que você nunca imaginou.',
+      'brand': 'Sony',
+      'category': 'Electronics',
+      'price': 4000.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Logitech G-Series Gaming Mouse',
+      'image': '/images/g203.jpg',
+      'description':
+        'Logitech G Mouse para jogos com fio 203, 8.000 DPI, luz de efeito óptico arco-íris, 6 botões programáveis, memória integrada, mapeamento de tela, compatível com computador PC/Mac e laptop - preto',
+      'brand': 'Logitech',
+      'category': 'Electronics',
+      'price': 150.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    }
+  ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'app.apps.AppConfig',
 ]
 
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +63,37 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -103,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -115,9 +161,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+        BASE_DIR / 'static'
+]
+
+MEDIA_URL= '/images/'
+
+MEDIA_ROOT='static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS =True
